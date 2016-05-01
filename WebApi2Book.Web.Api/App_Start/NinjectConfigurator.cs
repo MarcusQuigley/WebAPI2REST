f38142aadee1fa50ec809275f725daaccbb5e567
MarcusQuigley <marcusquigley@gmail.com>
2016-05-01 18:12:32 -0400

    Corrected connection string Adding IActionTransactionHelper to DI container

--- WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -9,6 +9,7 @@ using Ninject.Activation;
 using WebApi2Book.Common;
 using WebApi2Book.Common.Logging;
 using WebApi2Book.Data.SqlServer.Mapping;
+using WebApi2Book.Web.Common;
 
 namespace WebApi2Book.Web.Api.App_Start
 {
@@ -45,6 +46,7 @@ namespace WebApi2Book.Web.Api.App_Start
 
             container.Bind<ISessionFactory>().ToConstant(sessionFactory);
             container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+            container.Bind<IActionTransactionHelper>().To<ActionTransactionHelper>().InRequestScope();
         }
 
         private ISession CreateSession(IContext context)
