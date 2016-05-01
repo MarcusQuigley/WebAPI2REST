e4281d8baa18cf480d6063564cbaba6c3d34503c
MarcusQuigley <marcusquigley@gmail.com>
2016-05-01 17:59:56 -0400

    Unit of work

---
+++ WebApi2Book.Web.Common/ActionTransactionHelper.cs
@@ -0,0 +1,67 @@
+﻿using NHibernate;
+using NHibernate.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+namespace WebApi2Book.Web.Common
+{
+    public class ActionTransactionHelper : IActionTransactionHelper
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        public ActionTransactionHelper(ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public bool TransactionHandled { get; private set; }
+        public bool SessionClosed { get; private set; }
+
+
+        public void BeginTransaction()
+        {
+            if (!CurrentSessionContext.HasBind(sessionFactory)) return;
+            
+            var session = sessionFactory.GetCurrentSession();
+            if (session != null)
+            {
+                session.BeginTransaction();
+            }
+        }
+
+        public void EndTransaction(HttpActionExecutedContext filterContext)
+        {
+            if (!CurrentSessionContext.HasBind(sessionFactory)) return;
+
+            var session = sessionFactory.GetCurrentSession();
+            
+            if (session == null) return;
+             if (!session.Transaction.IsActive) return;
+            
+            if (filterContext.Exception == null)
+            {
+                session.Flush();
+                session.Transaction.Commit();
+            }
+            else
+            {
+                session.Transaction.Rollback();
+            }
+            TransactionHandled = true;
+        }
+
+        public void CloseSession()
+        {
+            if (!CurrentSessionContext.HasBind(sessionFactory)) return;
+
+            var session = sessionFactory.GetCurrentSession();
+            session.Close();
+            session.Dispose();
+            CurrentSessionContext.Unbind(sessionFactory);
+            SessionClosed = true;
+        }
+    }
+}
---
+++ WebApi2Book.Web.Common/UnitOfWorkActionFilterAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApi2Book.Web.Common
+{
+    public class UnitOfWorkActionFilterAttribute : ActionFilterAttribute
+    {
+        public virtual IActionTransactionHelper ActionTransactionHelper
+        {
+            get { return WebContainerManager.Get<IActionTransactionHelper>(); }
+        }
+
+        public override bool AllowMultiple
+        {
+            get { return false; } //will only execute once per call
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            ActionTransactionHelper.BeginTransaction();
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            ActionTransactionHelper.EndTransaction(actionExecutedContext);
+            ActionTransactionHelper.CloseSession();
+        }
+    }
+}
