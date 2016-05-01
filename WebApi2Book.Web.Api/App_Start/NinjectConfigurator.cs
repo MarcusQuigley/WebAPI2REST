3747ed8fa4a879dbe81ae3386b8c2b20c01b890f
MarcusQuigley <marcusquigley@gmail.com>
2016-05-01 17:06:26 -0400

    Configure NHibernate to talk to DB

--- WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -1,7 +1,14 @@
-﻿using log4net.Config;
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using log4net.Config;
+using NHibernate;
+using Ninject.Web.Common;
+using NHibernate.Context;
 using Ninject;
+using Ninject.Activation;
 using WebApi2Book.Common;
 using WebApi2Book.Common.Logging;
+using WebApi2Book.Data.SqlServer.Mapping;
 
 namespace WebApi2Book.Web.Api.App_Start
 {
@@ -15,6 +22,7 @@ namespace WebApi2Book.Web.Api.App_Start
         private void AddBindings(IKernel container)
         {
             ConfigureLogging(container);
+            ConfigureNhibernate(container);
             container.Bind<IDateTime>().To<DateTimeAdapter>().InSingletonScope();
         }
 
@@ -22,7 +30,32 @@ namespace WebApi2Book.Web.Api.App_Start
         {
             XmlConfigurator.Configure();
             var logManager = new LogManagerAdapter();
-            container.Bind<ILogManager>().ToConstant(logManager); 
+            container.Bind<ILogManager>().ToConstant(logManager);
+        }
+
+        private void ConfigureNhibernate(IKernel container)
+        {
+            var sessionFactory = Fluently
+                .Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(
+                c => c.FromConnectionStringWithKey("WebApi2BookDb")))
+                .CurrentSessionContext("web")
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TaskMap>())
+                .BuildSessionFactory();
+
+            container.Bind<ISessionFactory>().ToConstant(sessionFactory);
+            container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+        }
+
+        private ISession CreateSession(IContext context)
+        {
+            var sessionFactory = context.Kernel.Get<ISessionFactory>();
+            if (!CurrentSessionContext.HasBind(sessionFactory))
+            {
+                var session = sessionFactory.OpenSession();
+                CurrentSessionContext.Bind(session);
+            }
+            return sessionFactory.GetCurrentSession();
         }
     }
 }
