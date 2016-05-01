f0686a599f805da4f423146a02b42ad66b1a26b9
MarcusQuigley <marcusquigley@gmail.com>
2016-05-01 15:41:56 -0400

    Setting up NInject functionality

---
+++ WebApi2Book.Common/Logging/LogManagerAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using log4net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Common.Logging
+{
+    public class LogManagerAdapter : ILogManager
+    {
+        public ILog GetLog(Type typeAssociatedWithRequestLog)
+        {
+           var log= LogManager.GetLogger(typeAssociatedWithRequestLog);
+           return log;
+        }
+    }
+}
